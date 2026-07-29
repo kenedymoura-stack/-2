06aa265d65a6f318935fcd457841f310069a4988
kenedymoura-stack <kenedy.moura@escola.pr.gov.br>
2026-07-29 07:38:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function mudarMensagem() {
+    const mensagem = document.getElementById("mensagem");
 
+    mensagem.innerHTML = "Parabéns! Você criou sua primeira interação com JavaScript.";
+
+    document.body.style.backgroundColor = "#2c3e50";
+}
